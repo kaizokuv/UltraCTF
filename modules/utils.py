b1bce01569b72aae50f8fdb85b42a096870994b0
Kaizokuv <luckjumanraj@gmail.com>
2025-08-14 15:59:05 +0800

    Update entire repo with latest changes

--- modules/utils.py
+++
@@ -1,6 +0,0 @@
-def is_printable(b):
-    try:
-        text = b.decode()
-        return all(32 <= ord(c) <= 126 or c in '\n\r\t' for c in text)
-    except UnicodeDecodeError:
-        return False
